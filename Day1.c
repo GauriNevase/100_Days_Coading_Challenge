db4006970fc59e3f421d5afec966fab7f7021113
GauriNevase <110117024+GauriNevase@users.noreply.github.com>
2022-09-15 23:48:40 +0530

    Add files via upload

---
+++ Day1.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+    int N=4;
+    int A[5];
+    int i,temp=0;
+    //printf("Enter the no of integers in array A :");
+     scanf("%d",&N);
+    for(i=0;i<N;i++)
+    {
+        //printf("Enter the data:");
+        scanf("%d",&A[i]);
+    }
+    /*for(i=0;i<N;i++)
+    {
+    printf("%d",A[i]);
+    }
+    printf(" \n");*/
+    for(i=0;i<N;i++)
+    { 
+        while (i<N/2)
+        {
+            temp=A[i];
+            A[i]=A[N-i-1];
+            A[N-i-1]=temp;
+            N--;
+            printf("%d ",A[i]);
+        }
+     
+        printf("%d",A[3]);
+    }
+    return 0;
+}
